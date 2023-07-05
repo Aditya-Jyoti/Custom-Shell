656d6a9f5606239f9690d53c3cfdd30d93380211
Aditya <aj.adityajyoti@gmail.com>
2023-07-05 21:04:25 +0530

    Edit ls command

--- src/commands/Ls.cpp
+++ src/commands/Ls.cpp
@@ -10,8 +10,6 @@ namespace Commands
 {
     void Ls::listContents(fs::path argPath)
     {
-        std::cout << std::endl;
-
         for (fs::directory_entry file : fs::directory_iterator(argPath))
         {
             if (fs::is_directory(file))
